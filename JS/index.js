54c2597aee320b1e0e20587da29881ab803de91b
jim60105 <jim60105@gmail.com>
2016-05-12 07:35:28 +0800

    v16.05.12.1

--- JS/index.js
+++ JS/index.js
@@ -2,7 +2,7 @@ var app = angular.module('app', []);
 app.controller("Controller", function($scope,$sce,$timeout){
     //先call bodyInit()
     $scope.init = function(){
-        $scope.amount = 1;
+        $scope.amount = 0;
         $scope.makeStock();
     }
 
@@ -12,13 +12,14 @@ app.controller("Controller", function($scope,$sce,$timeout){
         return $sce.trustAsHtml(html_code);
     };
 
-    $scope.makeStock = function(){
+    $scope.makeStock = function(callback){
         var color = new RColor;
         $scope.stepCount = 0;
+        $scope.stepList = [];
         $scope.stockA = [];
         $scope.stockB = [];
         $scope.stockC = [];
-        $scope.layout = $scope.renderHtml('Layout:<br>');
+        $scope.layout = $scope.renderHtml('');
         $scope.colorList = [];
         for(var i=0;i<$scope.amount;i++){
             $scope.stockA.push(i+1);
@@ -31,22 +32,30 @@ app.controller("Controller", function($scope,$sce,$timeout){
     }
 
     $scope.calculate = function(){
+        $scope.makeStock();
         $scope.move($scope.amount,$scope.stockA,$scope.stockC,$scope.stockB,['A','C','B']);
     }
 
     $scope.move = function(value,source,target,temp,letter){
         if(value==1){
-            target.unshift(source.shift());
+            //target.unshift(source.shift());
             $scope.layout=$scope.renderHtml($scope.layout+value+' : '+letter[0]+' → '+letter[1]+'<br>');
+            $scope.stepList.push(letter);
             $scope.stepCount ++;
         }else{
             $scope.move(value-1,source,temp,target,[letter[0],letter[2],letter[1]]);
 
-            target.unshift(source.shift());
+            //target.unshift(source.shift());
             $scope.layout=$scope.renderHtml($scope.layout+value+' : '+letter[0]+' → '+letter[1]+'<br>');
+            $scope.stepList.push(letter);
             $scope.stepCount ++;
             
             $scope.move(value-1,temp,target,source,[letter[2],letter[1],letter[0]]);
         }
+    }
+
+    $scope.next = function(){
+        var step = $scope.stepList.shift();
+        eval('$scope.stock'+step[1]+'.unshift($scope.stock'+step[0]+'.shift());');
     }
 });
--- index.php
+++ index.php
@@ -14,30 +14,48 @@
         <script src="library/rcolor.js"></script>
 
         <script src="JS/index.js"></script>
+        <style>
+            *{
+                font-family: "Noto Sans", "Noto Sans JP","Noto Sans TC","Noto Sans SC", "Microsoft JhengHei", "Heiti TC", sans-serif;
+            }
+        </style>
     </head>
-    <body style="text-align: center;margin:50px 50px 0px 0px " ng-controller="Controller" ng-init="init()">
-        <div class="container" style="width:100%;line-height: 22px;text-align: center">
-            <input ng-model="amount" type="number" style="width:50px;text-align:right" ng-change="makeStock()"></input>
-            <button ng-click="calculate()">RUN</button>
-            共{{stepCount}}步
+    <body style="text-align: center" ng-controller="Controller" ng-init="init()">
+        <div style="color:red;font-size:40px;margin-top:20px">河內塔問題</div>
+        <div class="container" style="width:100%;line-height: 22px;text-align: center;margin:20px 50px 0px 0px">
+            <form>
+                選擇層數：
+                <input ng-model="amount" type="number" style="width:50px;text-align:right" ng-submit="calculate()"></input>
+                <button ng-click="calculate()">RUN</button>
+                共{{stepCount}}步
+            </form>
             <table style="width:100%">
-                <tbody style="vertical-align:top">
+                <tbody style="vertical-align:bottom">
                     <tr>
-                        <td style="width:25%" ng-bind-html="layout">Layout:</td>
-                        <td style="width:25%;text-align: center">A<br>
-                            <div ng-style="{'width':layerWidth(layer,stockA.length),'margin':'0px auto','background-color': colorList[layer-1]}" ng-repeat="layer in stockA track by $index" ng-bind="{{layer}}"></div>
+                        <td>Layout:</td>
+                        <td>A</td>
+                        <td>B</td>
+                        <td>C</td>
+                    </tr>
+                    <tr>
+                        <td style="width:25%;vertical-align:top" ng-bind-html="layout"></td>
+                        <td style="width:25%;text-align: center">
+                            <div ng-style="{'width':layerWidth(layerA,amount),'margin':'0px auto','background-color': colorList[layerA-1]}" ng-repeat="layerA in stockA track by $index">&nbsp;</div>
                         </td>
-                        <td style="width:25%">B<br>
-                            <div ng-style="{'width':layerWidth(layer,stockB.length),'margin':'0px auto','background-color': colorList[layer-1]}" ng-repeat="layer in stockB track by $index" ng-bind="{{layer}}"></div>
+                        <td style="width:25%">
+                            <div ng-style="{'width':layerWidth(layerB,amount),'margin':'0px auto','background-color': colorList[layerB-1]}" ng-repeat="layerB in stockB track by $index">&nbsp;</div>
                         </td>
-                        <td style="width:25%">C<br>
-                            <div ng-style="{'width':layerWidth(layer,stockC.length),'margin':'0px auto','background-color': colorList[layer-1]}" ng-repeat="layer in stockC track by $index" ng-bind="{{layer}}"></div>
+                        <td style="width:25%">
+                            <div ng-style="{'width':layerWidth(layerC,amount),'margin':'0px auto','background-color': colorList[layerC-1]}" ng-repeat="layerC in stockC track by $index">&nbsp;</div>
                         </td>
                     </tr>
                 </tbody>
             </table>
-            <div style="margin-top:100px;float: right">
-                v16.05.12.0
+            <button ng-click="next()" ng-disabled="stepList.length<1" style="margin:0px auto">下一步</button><br>
+            <div style="margin-top:100px;float: right;text-align:right;margin-right:50px;font-size:14px">
+                版本號： v16.05.12.1<br>
+                Github : <a href="https://github.com/jim60105/hanoi">https://github.com/jim60105/hanoi</a>
+<!-- Facebook Badge START --><br><a href="https://www.facebook.com/jim60105" title="&#x9673;&#x921e;" target="_TOP"><img class="img" src="https://badge.facebook.com/badge/100000288615898.2549.1160939690.png" style="border: 0px;" alt="" /></a><!-- Facebook Badge END -->
             </div>
         </div>
     </body>
